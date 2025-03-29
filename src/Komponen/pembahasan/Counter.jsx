dbfdfee869410531839053d924b410aac68b08fc
silalahi-17 <aguspangondian04@gmail.com>
2025-03-29 17:13:39 +0700

    hapus classname di file counter.jsx karena  belum digunakan

--- src/Komponen/pembahasan/Counter.jsx
+++ src/Komponen/pembahasan/Counter.jsx
@@ -4,11 +4,9 @@ const Counter = () => {
   const [count, setCount] = useState(0); // State count
 
   return (
-    <div className="p-4 border rounded-lg shadow-md w-60 text-center">
-      <h2 className="text-xl font-bold">Counter: {count}</h2>
-      <button
-        className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
-        onClick={() => setCount(count + 1)}
+    <div>
+      <h2>Counter: {count}</h2>
+      <button onClick={() => setCount(count + 1)}
       >
         Tambah
       </button>
